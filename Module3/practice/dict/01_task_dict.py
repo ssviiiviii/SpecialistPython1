acf47b739d25a5f90d87e976fc157c8bd1ac6708
ssviiiviii <96447402+ssviiiviii@users.noreply.github.com>
2021-12-23 11:33:23 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -1,10 +1,11 @@
-# Дан словарь, содержащий данные о товаре из магазина
-# "price" - цена товара в валюте "currency"
-# "count" - количествотовара в магазине
-# Считая,что курс доллара равен dollar_rate
-# Вычислите цену всех товаров с названием "name" в долларах
-
-item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
+item = {
+    "name": "Кроссовки", 
+    "price": "7540.5", 
+    "currency": "rub", 
+    "count": "10"
+    
+}
 dollar_rate = 74.12
-
-# TODO: your code here
+if item.get("name") == "Кроссовки":
+    summ = float((item.get("price"))) * dollar_rate
+    print(f'{summ} $')
